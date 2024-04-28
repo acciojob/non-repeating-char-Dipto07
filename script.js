b86477dbc3a12be0820a255681b6db14bfbd14ca
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-28 07:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount = new Map();
+
+    for (let char of str) {
+        if (charCount.has(char)) {
+            charCount.set(char, charCount.get(char) + 1);
+        } else {
+            charCount.set(char, 1);
+        }
+    }
+
+    for (let char of str) {
+        if (charCount.get(char) === 1) {
+            return char;
+        }
+    }
+
+    return null;
 }
-const input = prompt("Enter a string");
+ 
+const str = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
